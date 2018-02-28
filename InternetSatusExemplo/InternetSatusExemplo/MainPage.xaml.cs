bd4516221a3146f2a482278bf86d44ae53c83d60
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-02-28 13:47:44 -0300

    Primeira versão

--- InternetSatusExemplo/InternetSatusExemplo/MainPage.xaml.cs
+++ InternetSatusExemplo/InternetSatusExemplo/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Connectivity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ namespace InternetSatusExemplo
 		public MainPage()
 		{
 			InitializeComponent();
-		}
+
+            btnTeste.Clicked +=  (sender, e) => {
+
+                lblConexao.Text = CrossConnectivity.Current.IsConnected ? "Conectado" : "Coloque créditos";
+            };
+        }
 	}
 }
